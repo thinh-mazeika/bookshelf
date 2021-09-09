07ed9c9726eefb00899ae6b1fdc6d7ea9a6645c4
thinh-mazeika <thinh.mazeika@beckshybrids.com>
2021-09-09 16:34:53 -0400

    handles failed request

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,16 +1,21 @@
 function client(endpoint, customConfig = {}) {
-  // 🐨 create the config you'll pass to window.fetch
-  //    make the method default to "GET"
-  // 💰 if you're confused by this, that's fine. Scroll down to the bottom
-  // and I've got some code there you can copy/paste.
-  // 🐨 call window.fetch(fullURL, config) then handle the json response
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-  // 💰 here's how to get the full URL: `${process.env.REACT_APP_API_URL}/${endpoint}`
+  const fullURL = `${process.env.REACT_APP_API_URL}/${endpoint}`
+  const config = {
+    method: 'GET',
+    ...customConfig,
+  }
+  return window.fetch(fullURL, config).then(async response => {
+    const data = await response.json()
+    if (response.ok) {
+      return data
+    } else {
+      return Promise.reject(data)
+    }
+  })
 }
 
-export {client}
+export { client }
 
-/*
 
 
 
