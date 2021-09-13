4b6d33783a33b6b8a3bc5ae8028c3de5f44016ee
thinh-mazeika <thinh.mazeika@beckshybrids.com>
2021-09-13 15:51:02 -0400

    useBookSearch in discover

--- src/screens/discover.exercise.js
+++ src/screens/discover.exercise.js
@@ -1,45 +1,20 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
 import Tooltip from '@reach/tooltip'
-import bookPlaceholderSvg from 'assets/book-placeholder.svg'
 import {BookRow} from 'components/book-row'
 import {BookListUL, Input, Spinner} from 'components/lib'
 import * as React from 'react'
 import {FaSearch, FaTimes} from 'react-icons/fa'
-import {useQuery} from 'react-query'
 import * as colors from 'styles/colors'
-import {client} from 'utils/api-client'
-
-const loadingBook = {
-  title: 'Loading...',
-  author: 'loading...',
-  coverImageUrl: bookPlaceholderSvg,
-  publisher: 'Loading Publishing',
-  synopsis: 'Loading...',
-  loadingBook: true,
-}
-
-const loadingBooks = Array.from({length: 10}, (v, index) => ({
-  id: `loading-book-${index}`,
-  ...loadingBook,
-}))
+import {useBookSearch} from 'utils/books'
 
 function DiscoverBooksScreen({user}) {
   const [query, setQuery] = React.useState('')
   const [queried, setQueried] = React.useState(false)
-  const {
-    data: books = loadingBooks,
-    error,
-    isLoading,
-    isError,
-    isSuccess,
-  } = useQuery({
-    queryKey: ['bookSearch', {query}],
-    queryFn: () =>
-      client(`books?query=${encodeURIComponent(query)}`, {
-        token: user.token,
-      }).then(data => data.books),
-  })
+  const {books, error, isLoading, isError, isSuccess} = useBookSearch(
+    query,
+    user,
+  )
 
   function handleSearchSubmit(event) {
     event.preventDefault()
