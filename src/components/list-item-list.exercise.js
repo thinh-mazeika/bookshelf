4561b3dfc1fe134029e59c7aae4bd6f090d0d57e
thinh-mazeika <thinh.mazeika@beckshybrids.com>
2021-09-13 13:46:57 -0400

    list-item-list

--- src/components/list-item-list.exercise.js
+++ src/components/list-item-list.exercise.js
@@ -1,10 +1,9 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
-
-// 🐨 you'll need useQuery from 'react-query'
-// 🐨 and client from 'utils/api-client'
-import {BookListUL} from './lib'
+import {useQuery} from 'react-query'
+import {client} from 'utils/api-client'
 import {BookRow} from './book-row'
+import {BookListUL} from './lib'
 
 function ListItemList({
   user,
@@ -12,12 +11,11 @@ function ListItemList({
   noListItems,
   noFilteredListItems,
 }) {
-  // 🐨 call useQuery to get the list-items from the 'list-items' endpoint
-  // queryKey should be 'list-items'
-  // queryFn should call the 'list-items' endpoint
-
-  // 🐨 assign this to the list items you get back from react-query
-  const listItems = null
+  const {data: listItems} = useQuery({
+    queryKey: 'list-items',
+    queryFn: () =>
+      client(`list-items`, {token: user.token}).then(data => data.listItems),
+  })
 
   const filteredListItems = listItems?.filter(filterListItems)
 
