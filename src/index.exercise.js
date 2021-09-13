4388c3d2188c834d2a877d28107d812a67f936d2
thinh-mazeika <thinh.mazeika@beckshybrids.com>
2021-09-13 16:55:28 -0400

    handles error with queryConfig

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,27 @@
-import {loadDevTools} from './dev-tools/load'
-import './bootstrap'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
+import {ReactQueryConfigProvider} from 'react-query'
 import {App} from './app'
+import './bootstrap'
+import {loadDevTools} from './dev-tools/load'
+
+const queryConfig = {
+  queries: {
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false,
+    retry(failureCount, error) {
+      if (error.status === 404) return false
+      else if (failureCount < 2) return true
+      else return false
+    },
+  },
+}
 
 loadDevTools(() => {
-  ReactDOM.render(<App />, document.getElementById('root'))
+  ReactDOM.render(
+    <ReactQueryConfigProvider config={queryConfig}>
+      <App />
+    </ReactQueryConfigProvider>,
+    document.getElementById('root'),
+  )
 })
