1e4f365db078e80f0fb2d955aba16d49707354da
thinh-mazeika <thinh.mazeika@beckshybrids.com>
2021-09-16 15:48:00 -0400

    unit test for pure function

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,5 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from '../misc'
 
+test('formatDate formats the date to look nice', () => {
+  expect(formatDate(new Date('September 14, 1991'))).toBe('Sep 91')
+})
