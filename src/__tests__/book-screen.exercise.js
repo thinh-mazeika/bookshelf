5a06ecbd6b3d5f0792ff6a2d872da8081d0a9316
thinh-mazeika <thinh.mazeika@beckshybrids.com>
2021-09-20 14:45:28 -0400

    testing book screen

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,36 +1,65 @@
-// 🐨 here are the things you're going to need for this test:
-// import * as React from 'react'
-// import {render, screen, waitFor} from '@testing-library/react'
-// import {queryCache} from 'react-query'
-// import {buildUser, buildBook} from 'test/generate'
-// import * as auth from 'auth-provider'
-// import {AppProviders} from 'context'
-// import {App} from 'app'
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import {App} from 'app'
+import * as auth from 'auth-provider'
+import {AppProviders} from 'context'
+import * as React from 'react'
+import {queryCache} from 'react-query'
+import {buildBook, buildUser} from 'test/generate'
 
-// 🐨 after each test, clear the queryCache and auth.logout
+afterEach(async () => {
+  queryCache.clear()
+  await auth.logout()
+})
 
-test.todo('renders all the book information')
-// 🐨 "authenticate" the client by setting the auth.localStorageKey in localStorage to some string value (can be anything for now)
-
-// 🐨 create a user using `buildUser`
-// 🐨 create a book use `buildBook`
-// 🐨 update the URL to `/book/${book.id}`
-//   💰 window.history.pushState({}, 'page title', route)
-//   📜 https://developer.mozilla.org/en-US/docs/Web/API/History/pushState
-
-// 🐨 reassign window.fetch to another function and handle the following requests:
-// - url ends with `/bootstrap`: respond with {user, listItems: []}
-// - url ends with `/list-items`: respond with {listItems: []}
-// - url ends with `/books/${book.id}`: respond with {book}
-// 💰 window.fetch = async (url, config) => { /* handle stuff here*/ }
-// 💰 return Promise.resolve({ok: true, json: async () => ({ /* response data here */ })})
-
-// 🐨 render the App component and set the wrapper to the AppProviders
-// (that way, all the same providers we have in the app will be available in our tests)
-
-// 🐨 use waitFor to wait for the queryCache to stop fetching and the loading
-// indicators to go away
-// 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitfor
-// 💰 if (queryCache.isFetching or there are loading indicators) then throw an error...
-
-// 🐨 assert the book's info is in the document
+test('renders all the book information', async () => {
+  window.localStorage.setItem(auth.localStorageKey, '__auth_provider_token__')
+  const user = buildUser()
+  const book = buildBook()
+  const route = `book/${book.id}`
+  window.history.pushState({}, 'Test Page', route)
+  const originalFetch = window.fetch
+  window.fetch = async (url, config) => {
+    if (url.endsWith(`/bootstrap`)) {
+      return {
+        ok: true,
+        json: async () => ({
+          user: {...user, token: '__auth_provider_token__'},
+          listItems: [],
+        }),
+      }
+    } else if (url.endsWith(`/book/${book.id}`)) {
+      return {
+        ok: true,
+        json: async () => ({
+          book,
+        }),
+      }
+    }
+    return originalFetch(url, config)
+  }
+  render(<App />, {wrapper: AppProviders})
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+  expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
+  expect(screen.getByText(book.author)).toBeInTheDocument()
+  expect(screen.getByText(book.author)).toBeInTheDocument()
+  expect(screen.getByText(book.synopsis)).toBeInTheDocument()
+  expect(screen.getByRole('img', {name: /book cover/i})).toHaveAttribute(
+    'src',
+    book.coverImageUrl,
+  )
+  expect(screen.getByRole('button', {name: /add to list/i})).toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /remove from list/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as read/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as unread/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('textbox', {name: /notes/i}),
+  ).not.toBeInTheDocument()
+  expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
+  expect(screen.queryByLabelText(/start date/i)).not.toBeInTheDocument()
+})
